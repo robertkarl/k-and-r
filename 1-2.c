eacff1eb17997208beb086bf6932a590b7320eaf
Robert Karl <robertkarljr@gmail.com>
2017-04-08 18:44:44 -0700

    First assignments

---
+++ 1-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+	printf("hello world \n"); 
+	/* \a has no effect, c, d, g, h, i, j, k unrecognized 
+	/* r erases a tab? */
+	/* e deletes the next character */
+	/* f inserts a newline and whitespace to old position */
+	printf("hello world will be erased \H and some more stuff\n"); 
+	printf("hello world again \n"); 
+}
