2d6f73842f18c3bf487f8604dcdc5e43e9e2a1b6
Robert Karl <robertkarljr@gmail.com>
2017-04-19 06:55:14 -0700

    Added fold tests

--- 1-22-fold.c
+++ 1-22-fold.c
@@ -1,6 +1,10 @@
 /* fold long input lines into shorter lines after the last non-blank 
 character that occurs before the n-th column of input.
 
+* opening whitespace is left alone
+
+* A split always occurs on a blank, the blank is converted to a newline
+
 * If the input contains lines longer than MAXLINE characters, behavior is 
 	undefined (further input may be ignored)
 
@@ -10,7 +14,7 @@ character that occurs before the n-th column of input.
 */
 
 #define MAXLINE 1000
-#define FOLDN 81
+#define FOLDN 80
 
 #include <stdio.h>
 #include <string.h>
@@ -20,32 +24,42 @@ int cansplit(char s)
 	return s == '\t' || s == ' ';
 }
 
+int findsplit(char line[], int i, int max_nchars)
+{
+	while (i > 0 && !cansplit(line[i])) {
+		--i;
+	}
+	while (i > 0 && line[i - 1] == ' ')
+		--i;
+	if (i == 0)
+		return FOLDN;
+	return i;
+}
+
 void splitline(char line[], int len)
 {
 	int i;
 	int start = 0;
 
- 	/* 82 for example, if line length is 80. 80 + newline + null byte */
-	char toprint[FOLDN + 1];
+ 	/* include space for newline and null byte */
+	char toprint[FOLDN + 2];
 
 	i = FOLDN - 1;
 	while (start < len) {
 		/* a "full line" has length FOLDN + 1 including the newline      */
-		if (len - start <= FOLDN) {
+		if (len - start <= FOLDN + 1) {
 			printf("%s", line + start);
 			start = len;
 		}
 		else {
-			while (i > 0 && !cansplit(line[i])) {
-				--i;
-			}
-			if (i == 0)
-				i = FOLDN - 1;
+			i = findsplit(line, start + FOLDN, FOLDN);
 			strncpy(toprint, line + start, i);
 			toprint[i] = '\n';
 			toprint[i + 1] = '\0';
 			printf("%s", toprint);
 			start = start + i;
+			if (line[i] == ' ')
+				++start;
 		}
 	}
 }
--- Makefile
+++ Makefile
@@ -11,6 +11,14 @@ entab: 1-21-entab.c get_line.c
 fold: 1-22-fold.c get_line.c
 	cc $(CFLAGS) get_line.c 1-22-fold.c -o fold
 
+testfold: fold
+	./fold < test.fold.1.in > test.fold.1.out
+	diff test.fold.1.out test.fold.1.out.expected
+	./fold < test.fold.2.in > test.fold.2.out
+	diff test.fold.2.out test.fold.2.out.expected
+	./fold < test.fold.3.in > test.fold.3.out
+	diff test.fold.3.out test.fold.3.out.expected
+
 clean:
 	rm -f entab detab
 
