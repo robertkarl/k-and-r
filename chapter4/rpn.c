8ca7b74a42c243648817c3515b4163f2973af302
Robert Karl <robertkarljr@gmail.com>
2017-06-23 01:40:35 -0700

    exercise 4.03: add % and negative number support

--- chapter4/rpn.c
+++ chapter4/rpn.c
@@ -22,7 +22,109 @@ main()
 		case '+':
 			push(pop() + pop());
 			break;
+		case '*':
+			push(pop() * pop());
+			break;
 		case '-':
 			op2 = pop();
 			push(pop() - op2);
 			break;
+		case '%':
+			op2 = pop();
+			if (op2 != 0.0)
+				push((int)pop() % (int)op2);
+			else
+				printf("error: zero modulus\n");
+			break;
+		case '/':
+			op2 = pop();
+			if (op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+		default:
+			printf("error: unknown command %s\n", s);
+			break;
+		}
+	}
+	return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: pop from empty stack\n");
+		return 0;
+	}
+}
+
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+	int c, tmp;
+	int i = 0;
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+
+	/* Lookahead one to check for negative numbers */
+	if (tmp = getch(), c == '-' && isdigit(tmp)) {
+		c = tmp;
+		s[++i] = c;
+	}
+	else 
+		ungetch(tmp);
+
+	if (!isdigit(c) && c != '.')
+		return c;
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+	
+#define BUFSIZE 100
+
+int bufp = 0;
+char buf[BUFSIZE];
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+
